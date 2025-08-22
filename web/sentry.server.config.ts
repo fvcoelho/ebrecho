e8ecdad18ed378e818c5fbb675c856e2d004c66c
Coelho <fvcoelho@gmail.com>
2025-08-22 11:32:22 -0300

    add sentry

---
+++ web/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://64faeac2e784eb5e5c992d0a5ac70422@o4509887683231744.ingest.us.sentry.io/4509888203259904",
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
